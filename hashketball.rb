0a63c16f2c3cf1dc24e4d64668a6bef317d50a8c
Marisa You <marisa.you@gmail.com>
2020-09-01 21:40:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,28 @@
-# Write your code here!
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {
+          :player_name => 
+          :number =>
+          :shoe =>
+          :points =>
+          :rebounds =>
+          :assists =>
+          :steals =>
+          :blocks =>
+          :slam_dunks =>
+        },
+        {
+          
+        },
+      ]
+    },
+    :away => {
+      :team_name =>
+    }
+  }
+
+  
